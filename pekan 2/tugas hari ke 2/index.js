ee716f4ec559a6fb27a9c0f93b2ed46e684e160f
Sahrul-ux <sahrulrajab11@gmail.com>
2020-07-28 22:29:57 +0700

    belajar asyncronous

---
+++ pekan 2/tugas hari ke 2/index.js
@@ -0,0 +1,21 @@
+// di index.js
+var readBooks = require('./callback.js')
+ 
+var books = [
+    {name: 'LOTR', timeSpent: 3000}, 
+    {name: 'Fidas', timeSpent: 2000}, 
+    {name: 'Kalkulus', timeSpent: 4000},
+    {name: 'komik', timeSpent: 1000}
+]
+ 
+// Tulis code untuk memanggil function readBooks di sini
+
+readBooks(1000, books[0], function (sisaWaktu){
+    readBooks(sisaWaktu, books[1], function (sisaWaktu){
+        readBooks(sisaWaktu, books[2], function (sisaWaktu) {
+            readBooks(sisaWaktu, books[3], function (sisaWaktu) {
+
+            })
+        })
+    })
+})
